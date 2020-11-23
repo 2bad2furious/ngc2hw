3f5fb204790e02ea7da6132faa6004b3afac45e3
Martin Macura <mr.martin.macura@gmail.com>
2020-11-23 16:07:18 +0100

    actual stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngc2hw';
+
+  chemeRender = true;
+
+  names = ['Vítek', 'Marco', 'Zdeněk'];
+
+  printName(name: string): void {
+    console.debug(name);
+  }
 }
